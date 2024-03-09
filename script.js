52bd61014b0ab1b3b3c110e1e04957e050dd20a3
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-03-09 15:02:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var num6=parseInt(prompt("Enter first Number"));
+var num7=parseInt(prompt("Enter second Number"));
+var num8=parseInt(prompt("Enter third Number"));
+
+var num9=(num6+num7+num8);
+alert(num9);
+
+
+var num2=(num9*10)
+alert(9*10);
